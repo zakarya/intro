0413a419fc1600949dcb29f5d15c8da594ef0cd7
Zack Vera <qphys50@gmail.com>
2014-01-01 22:04:51 -0700

    Exercises create form

--- app/scripts/directives/exercises.js
+++ app/scripts/directives/exercises.js
@@ -5,6 +5,22 @@ angular.module('introApp')
     return {
       templateUrl: 'views/partials/exercises.html',
       restrict: 'A',
+      controller: ['$scope', 'ExerciseService', function ($scope, ExerciseService) {
+
+        function _createExercise () {
+          ExerciseService.create($scope.exercise).then(function (data) {
+            $scope.exercises.unshift(data);
+          });
+        }
+
+        function _initialize() {
+          //Setup Scope Variables
+          $scope.exercise = {};
+          $scope.createExercise = _createExercise;
+        }
+
+        _initialize();
+      }],
       link: function () {
       }
     };
