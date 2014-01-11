cacc60b1bd9ad95e562f7760958856f9cb31b8c3
Zack Vera <qphys50@gmail.com>
2014-01-11 14:11:17 -0700

    Progress pane

--- app/scripts/directives/progress.js
+++ app/scripts/directives/progress.js
@@ -1,10 +1,25 @@
 'use strict';
 
 angular.module('introApp')
-  .directive('progress', [function () {
+  .directive('progress', ['$rootScope', 'UserService', function ($rootScope, UserService) {
     return {
       templateUrl: 'views/partials/progress.html',
       restrict: 'A',
+      controller: ['$scope', function ($scope) {
+        function _getProgress() {
+          UserService.find($rootScope.user.id).then(function (data) {
+            $scope.user.exercises = data.exercises;
+          });
+        }
+
+        function _initialize() {
+          $scope.getProgress = _getProgress;
+          $scope.user.exercises = {};
+          $scope.getProgress();
+        }
+
+        _initialize();
+      }],
       link: function () {
       }
     };
